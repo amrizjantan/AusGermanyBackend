2d37c149bf7568e426059023a987be9aa4d90fbc
sauerbulba <sauerbulba@proton.me>
2024-09-16 21:29:42 +0200

    fix User model camelcase resetPasswordToken

--- models/User.js
+++ models/User.js
@@ -19,7 +19,7 @@ const UserSchema = new mongoose.Schema({
     required: true,
     minlength: 6,
   },
-  resetpasswordtoken: String,
+  resetPasswordToken: String,
   resetPasswordExpires: Date,
   urls: [
     {
